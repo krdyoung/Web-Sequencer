93fe10eef7969f5346edc7ba602d999022aaae3e
krdyoung <krd.young@gmail.com>
2020-05-07 12:20:58 +0100

    Update DrumController.js

--- DrumController.js
+++ DrumController.js
@@ -1,9 +1,9 @@
 class DrumController {
   constructor() {
-    this.kick = new Tone.Player("/Samples/kick.wav").toMaster();
-    this.snare = new Tone.Player("/Samples/snare.wav").toMaster();
-    this.clap = new Tone.Player("/Samples/clap.wav").toMaster();
-    this.hat = new Tone.Player("/Samples/hat.wav").toMaster();
+    this.kick = new Tone.Player("/Web-Sequencer/Samples/kick.wav").toMaster();
+    this.snare = new Tone.Player("/Web-Sequencer/Samples/snare.wav").toMaster();
+    this.clap = new Tone.Player("/Web-Sequencer/Samples/clap.wav").toMaster();
+    this.hat = new Tone.Player("/Web-Sequencer/Samples/hat.wav").toMaster();
   }
   playKick() {
     this.kick.start();
